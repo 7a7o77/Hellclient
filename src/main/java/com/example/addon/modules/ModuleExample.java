a57988fa822210c3196e59c3cd71aa4e3ab8e0e7
7a7o77 <ahlenlol3rby@gmail.com>
2023-08-21 13:43:01 +0200

    test

--- src/main/java/com/example/addon/modules/ModuleExample.java
+++ src/main/java/com/example/addon/modules/ModuleExample.java
@@ -1,26 +1,29 @@
 package com.example.addon.modules;
 
 import com.example.addon.Addon;
+import meteordevelopment.meteorclient.events.entity.player.InteractEntityEvent;
 import meteordevelopment.meteorclient.settings.BoolSetting;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.orbit.EventHandler;
+import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket;
+
 
 public class ModuleExample extends Module {
-    private final Setting<Boolean> hello;
     public ModuleExample() {
-        super(Addon.CATEGORY, "example", "An Custom module in a custom category.");
-        SettingGroup settingGroup = settings.getDefaultGroup();
-        hello = settingGroup.add(new BoolSetting.Builder()
-            .defaultValue(false)
-            .name("test")
-            .description("testing")
-            .build()
-        );
+        super(Addon.CATEGORY, "rhit", "An Custom module in a custom category.");
 
     }
     @Override
     public void onActivate(){
-
+        super.onActivate();
+    }
+    @EventHandler
+    void test(InteractEntityEvent e){
+        if (isActive()){
+            e.setCancelled(true);
+            mc.player.attack(e.entity);
+        }
     }
 }
